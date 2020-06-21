ef6392404fe431351703dea7bc71ca2d2ed2d178
DTLA1215-code <re0rems@gmail.com>
2020-06-21 23:23:00 +0800

    itheimaAboutHealth_day01

---
+++ health_parent/health_dao/src/main/java/com/itheima/health/dao/CheckItemDao.java
@@ -0,0 +1,10 @@
+package com.itheima.health.dao;
+
+import com.itheima.health.pojo.CheckItem;
+
+import java.util.List;
+
+public interface CheckItemDao {
+
+    List<CheckItem>findAll();
+}
---
+++ health_parent/health_web/src/main/java/com/itheima/health/controller/CheckItemController.java
@@ -0,0 +1,26 @@
+package com.itheima.health.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.health.constant.MessageConstant;
+import com.itheima.health.entity.Result;
+import com.itheima.health.pojo.CheckItem;
+import com.itheima.health.service.CheckItemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("checkitem")
+public class CheckItemController {
+
+    @Reference
+    private CheckItemService checkItemService;
+
+    @GetMapping("findAll")
+    public Result findAll(){
+        List<CheckItem> list = checkItemService.findAll();
+        return new Result(true, MessageConstant.QUERY_CHECKITEM_SUCCESS,list);
+    }
+}
